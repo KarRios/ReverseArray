f09a34ff8fd140d75b3a307fa828923c614d9e5d
KarRios <43351714+KarRios@users.noreply.github.com>
2019-02-05 17:19:38 -0800

    Create reverse.cpp

---
+++ reverse.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<array>
+using namespace std;
+
+int main(){
+int array[100]; //creates an array with 100 elements max.
+int size = 0;
+
+cout << "Enter array size: ";
+cin >> size;
+cout << endl;
+
+cout << "Enter array elements: ";
+for(int i = 0; i < size; ++i){
+  cin >> array[i];
+ }
+ 
+cout << endl;
+
+reverseArray(array,size);
+
+cout << "New array is: ";
+for(int k = 0; k < size; ++k){
+  cout << array[k];
+ }
+cout << endl;
+
+}
+
+void reverseArray(int arr[], int length){
+  for(int j = 0; j < (length/2); ++j){
+    int temp = arr[j];
+    arr[j] = arr[length-j];
+    arr[length-j] = temp;
+  }
+}
